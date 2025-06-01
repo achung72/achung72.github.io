e0d3c555c3b40c00845c288642fdb9a31906fc70
achung72 <alanchung2000@gmail.com>
2025-06-01 03:10:29 +0000

    Deploying to gh-pages from @ achung72/achung72.github.io@4031ac1e4d5984807de79de872d80c771785c8bc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -68,9 +68,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-function-of-salt-why-is-my-food-bland",
+          title: 'Function of Salt (why is my food bland?)',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-1",
