f120a69e80f565c0f3d374cf09f3e0022cb75b46
achung72 <alanchung2000@gmail.com>
2026-08-07 02:55:24 +0000

    Deploying to gh-pages from @ achung72/achung72.github.io@191f08d6611bfef349d9b761d9aec5240979f0e5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "My CV is in the upper-right corner. A brief summary is here",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
